0e2a78bf2b4076f7ddbb0ae172af48d2fada7a6b
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-17 19:55:35 -0300

    Adds requirement #5

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const balls = Array.from(document.getElementsByClassName('ball'));
 const rgbColor = document.getElementById('rgb-color');
 const allColors = [];
+const answer = document.getElementById('answer');
 
 function randomizeColors() {
   const firstRandomNumber = Math.floor(Math.random() * 255);
@@ -28,4 +29,30 @@ window.onload = function initialize() {
     balls[counter].style.background = `rgb(${newColor[0]}, ${newColor[1]}, ${newColor[2]}`;
   }
   addChallengeColor();
+  answer.innerText = 'Escolha uma cor';
 };
+
+function checkAnswer(ball) {
+  const ballColor = ball.style.background;
+  const challengeColor = `rgb${rgbColor.innerText}`;
+  if (ballColor === challengeColor) {
+    answer.innerText = 'Acertou!';
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
+  }
+}
+
+function addSelected(originEvent) {
+  const selectedBall = originEvent.target;
+  for (let counter = 0; counter < balls.length; counter += 1) {
+    if (balls[counter].classList.contains('selected')) {
+      balls[counter].classList.remove('selected');
+    } else if (balls[counter] === selectedBall) {
+      selectedBall.classList.add('selected');
+      checkAnswer(selectedBall);
+    }
+  }
+}
+for (let counter = 0; counter < balls.length; counter += 1) {
+  balls[counter].addEventListener('click', addSelected);
+}
