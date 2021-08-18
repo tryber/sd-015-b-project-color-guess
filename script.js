a24bbecd0c984cf1c2d533d26e50221a89e278fd
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-18 00:47:00 -0300

    Implementa chamada de funçoes iniciais dentro da resetarJogo();

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function geraCor() {
     elementosAColorir[index].style.backgroundColor = randomColor;
   }
 }
-geraCor();
 
 function selecionaElemento() {
   // Captura elementos de Cores para pegar seu tamanho
@@ -18,21 +17,24 @@ function selecionaElemento() {
   elementoDiv.innerText = valorRGB;
   // console.log(randomElement); // [DEBUG]
 }
-selecionaElemento();
 
 function addEventClick() {
     // Adiciona evento de clique na class ball quando a página é carregada
     const elementosColoridos = document.getElementsByClassName('ball');
     const elementoDiv = document.getElementById('rgb-color');
+    const elementoResult = document.getElementById('answer');
     for (let index = 0; index < elementosColoridos.length; index +=1) {
         elementosColoridos[index].addEventListener('click', function () {
           // console.log('Função click foi adicionada ao elemento ' + index);
           // Ao clicar no elemento, roda função que compara se o texto dentro do rgb-color é igual ao backgroundColor do Elemento.
           const propriedadeDoElemento = event.target.style.backgroundColor;
           if (propriedadeDoElemento === elementoDiv.innerText) {
-              console.log('É IGUAL!!!');
+            // console.log('É IGUAL!!!'); // [DEBUG]
+            elementoResult.innerText = 'Acertou!';
+            resetarJogo();
           } else {
-            console.log('NÃO É IGUAL!!!');
+            // console.log('NÃO É IGUAL!!!'); // [DEBUG]
+            elementoResult.innerText = 'Errou! Tente novamente!';
           }
           
           // Deve-se incrementar 3 pontos a cada acerto no Placar, caso a cor seja acertada"
@@ -41,11 +43,19 @@ function addEventClick() {
 }
 addEventClick();
 
+function resetarJogo() {
+  const elementoResult = document.getElementById('answer');
+  elementoResult.innerText = 'Escolha uma cor';
+  geraCor();
+  selecionaElemento();
+}
+resetarJogo();
+
 // Adiciona ação a botões:
 const botaoDeReset = document.getElementById('reset-game');
 botaoDeReset.addEventListener('click', function () {
     // Quando clicar no botão de Reset
     // Chama-se a função que troca de Cor.
-    geraCor();
+    resetarJogo();
 
 });
