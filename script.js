3314d7699f07bab79628d73a596c12b41f6b5c90
Paulo Flóra <tflora.2004@gmail.com>
2021-08-22 14:47:29 -0300

    Feito o requisito 5.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const guesses = document.querySelectorAll('.ball');
+const answerText = document.getElementById('answer');
+const randomBall = Math.floor(Math.random() * 6);
+const answer = 'rgb(168, 34, 1)';
 
 function generateRandomColor() {
   const values = '0123456789ABCDEF';
@@ -10,12 +13,23 @@ function generateRandomColor() {
   return randomColor;
 }
 
-const randomBall = Math.floor(Math.random() * 6);
-
-guesses[randomBall].style.background = 'rgb(168, 34, 1)';
+guesses[randomBall].style.background = answer;
 
 for (let i = 0; i < guesses.length; i += 1) {
   if (guesses[i].style.background !== 'rgb(168, 34, 1)') {
     guesses[i].style.background = generateRandomColor();
   }
+}
+
+function checkGuess(event) {
+  const guess = event.target.style.background;
+  if (guess === answer) {
+    answerText.innerText = 'Acertou!';
+  } else {
+    answerText.innerText = 'Errou! Tente novamente!';
+  }
+}
+
+for (let i = 0; i < guesses.length; i += 1) {
+  guesses[i].addEventListener('click', checkGuess);
 }
