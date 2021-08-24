797f1a76324b234342c03298d4402d6bdea6ba04
Flávio Bianchetti <fjb.developer@gmail.com>
2021-08-24 08:29:55 -0300

    Atendido requisito 5 - validado via npm

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myColor = document.getElementById('rgb-color');
 const myBall = document.getElementsByClassName('ball');
+const optionColors = document.getElementById('option-colors');
+const answer = document.getElementById('answer');
 
 function generateRandomNumber() {
   return Math.floor(Math.random() * 255).toString(10);
@@ -22,14 +24,43 @@ function chosenColor() {
 }
 
 function paintBalls() {
+  const num = Math.floor(Math.random() * 4);
   for (let index = 0; index < myBall.length; index += 1) {
     let color = 'rgb';
-    color += generateRandomColor();
+    if (index === num) {
+      color += myColor.innerText;
+    } else {
+      color += generateRandomColor();
+    }
     myBall[index].style.backgroundColor = color;
   }
+}
+
+function checkColor(object) {
+  let color = 'rgb';
+  color += myColor.innerText;
+  if (color === object.target.style.backgroundColor) {
+    return true;
+  }
+  return false;
+}
+
+function colorClick(event) {
+  if (event.target.className === 'ball') {
+    if (checkColor(event)) {
+      answer.innerText = 'Acertou!';
+    } else {
+      answer.innerText = 'Errou! Tente novamente!';
+    }
+  }
+}
+
+function selectColorClick() {
+  optionColors.addEventListener('click', colorClick);
 }
 
 window.onload = function onloadPage() {
   chosenColor();
   paintBalls();
+  selectColorClick();
 };
