0acd8293c51b6c0b6eaa1f56f249b500434757d7
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-05 11:55:31 -0300

    Requisito 4 completo

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+// Gera um número aleatório entre 1 e 255 para o RGB.
+function generateRandomNumber() {
+  return Math.random() * (255 - 1) + 1;
+}
+
+// Gera uma cor aleatória em RGB.
+function generateRandomRGB() {
+  const red = generateRandomNumber();
+  const green = generateRandomNumber();
+  const blue = generateRandomNumber();
+  const rgb = `rgb(${red},${green},${blue})`;
+  return rgb;
+}
+
 const createColor = () => {
   const color = document.createElement('div');
   color.className = 'ball';
+  color.style.backgroundColor = generateRandomRGB();
   return color;
 };
 
