a318617fce75b611f78b2d45d4ff4a7109dff7e5
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-05 12:00:11 -0300

    Refatorando e comentando

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Gera um número aleatório entre 1 e 255 para o RGB.
-function generateRandomNumber() {
+const generateRandomNumber = () => {
   return Math.random() * (255 - 1) + 1;
 }
 
 // Gera uma cor aleatória em RGB.
-function generateRandomRGB() {
+const generateRandomRGB = () => {
   const red = generateRandomNumber();
   const green = generateRandomNumber();
   const blue = generateRandomNumber();
@@ -12,17 +12,19 @@ function generateRandomRGB() {
   return rgb;
 }
 
-const createColor = () => {
+// Cria um círculo de seleção de cor
+const createColorSelector = () => {
   const color = document.createElement('div');
   color.className = 'ball';
   color.style.backgroundColor = generateRandomRGB();
   return color;
 };
 
+// Insere os seletores de cores na tela
 const insertColors = () => {
   const colorSelector = document.getElementById('color-selector');
   for (let i = 0; i < 6; i += 1) {
-    colorSelector.appendChild(createColor());
+    colorSelector.appendChild(createColorSelector());
   }
 };
 
