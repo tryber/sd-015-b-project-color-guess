663038c4b81c1a680cbe6d1bd2961babca37a201
DanielHott <daniel_hott@outlook.com>
2021-10-01 16:35:58 -0300

    Adicionando requisito 5

--- script.js
+++ script.js
@@ -4,15 +4,33 @@ function geraNumeros() {
   const gera3Termo = Math.floor(Math.random() * (255 - 0) - 0);
   return `rgb(${gera1Termo}, ${gera2Termo}, ${gera3Termo})`;
 }
-
-console.log(geraNumeros());
-
+const resposta = document.getElementById('answer');
 const rgbPadrao = document.getElementById('rgb-color');
 rgbPadrao.innerText = `${geraNumeros()}`;
+
+function averiguaCor(event) {
+  if (event.target.style.backgroundColor === rgbPadrao.innerText) {
+    resposta.innerText = 'Acertou!';
+  } else {
+    resposta.innerText = 'Errou! Tente novamente!';
+  }
+}
+
+function adicionaBotao(event) {
+  event.target.addEventListener('click', averiguaCor);
+}
+
 const balls = document.querySelectorAll('.ball');
 function geraBolas() {
   balls.forEach((element) => {
     element.style.backgroundColor = geraNumeros();
+    element.addEventListener('click', averiguaCor);
   });
 }
 geraBolas();
+
+function gerarCerto() {
+  balls[Math.floor(Math.random() * (6 - 1) + 1)].style.backgroundColor = rgbPadrao.innerText;
+}
+
+gerarCerto();
