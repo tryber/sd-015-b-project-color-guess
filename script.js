aabba01dbd6877d640fd73961991733a8f928695
DanielHott <daniel_hott@outlook.com>
2021-10-01 16:06:02 -0300

    Adicionando terceiro e quarto elemento

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ function geraNumeros() {
   const gera1Termo = Math.floor(Math.random() * (255 - 0) - 0);
   const gera2Termo = Math.floor(Math.random() * (255 - 0) - 0);
   const gera3Termo = Math.floor(Math.random() * (255 - 0) - 0);
-  return `${gera1Termo}, ${gera2Termo}, ${gera3Termo}`;
+  return `rgb(${gera1Termo}, ${gera2Termo}, ${gera3Termo})`;
 }
 
 console.log(geraNumeros());
 
 const rgbPadrao = document.getElementById('rgb-color');
-rgbPadrao.innerText = `(${geraNumeros()})`;
+rgbPadrao.innerText = `${geraNumeros()}`;
+const balls = document.querySelectorAll('.ball');
+function geraBolas() {
+  balls.forEach((element) => {
+    element.style.backgroundColor = geraNumeros();
+  });
+}
+geraBolas();
